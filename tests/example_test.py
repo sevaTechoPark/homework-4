4575b12bf09a4a80a465de87b7298210e7a7c442
seva <seva213415@inbox.ru>
2018-05-12 20:27:46 +0300

    auth complete

--- tests/example_test.py
+++ tests/example_test.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Page(object):
-    BASE_URL = 'http://tech-mail.ru/'
+    BASE_URL = 'http://ok.ru/'
     PATH = ''
 
     def __init__(self, driver):
@@ -31,13 +31,13 @@ class AuthPage(Page):
 
     @property
     def top_menu(self):
-        return TopMenu(self.driver)    
+        return TopMenu(self.driver)
 
 
 class CreatePage(Page):
     PATH = '/blog/topic/create/'
 
-    @property     
+    @property
     def form(self):
         return CreateForm(self.driver)
 
@@ -51,7 +51,7 @@ class TopicPage(Page):
 class BlogPage(Page):
     @property
     def topic(self):
-        return Topic(self.driver)  
+        return Topic(self.driver)
 
 
 class Component(object):
@@ -60,13 +60,9 @@ class Component(object):
 
 
 class AuthForm(Component):
-    LOGIN = '//input[@name="login"]'
-    PASSWORD = '//input[@name="password"]'
-    SUBMIT = '//span[text()="Войти"]'
-    LOGIN_BUTTON = '//a[text()="Вход для участников"]'
-
-    def open_form(self):
-        self.driver.find_element_by_xpath(self.LOGIN_BUTTON).click()
+    LOGIN = '//input[@name="st.email"]'
+    PASSWORD = '//input[@name="st.password"]'
+    SUBMIT = '//input[@value="Войти"]'
 
     def set_login(self, login):
         self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
@@ -84,7 +80,7 @@ class TopMenu(Component):
     def get_username(self):
         return WebDriverWait(self.driver, 30, 0.1).until(
             lambda d: d.find_element_by_xpath(self.USERNAME).text
-        )  
+        )
 
 
 class CreateForm(Component):
@@ -107,7 +103,7 @@ class CreateForm(Component):
 
     def set_short_text(self, short_text):
         self.driver.find_element_by_xpath(self.SHORT_TEXT).send_keys(short_text)
-    
+
     def set_main_text(self, main_text):
         self.driver.find_element_by_xpath(self.MAIN_TEXT).send_keys(main_text)
 
@@ -136,7 +132,7 @@ class Topic(Component):
         )
 
     def open_blog(self):
-        self.driver.find_element_by_xpath(self.BLOG).click()  
+        self.driver.find_element_by_xpath(self.BLOG).click()
 
     def delete(self):
         self.driver.find_element_by_xpath(self.DELETE_BUTTON).click()
@@ -148,7 +144,7 @@ class Topic(Component):
 
 class ExampleTest(unittest.TestCase):
     USERNAME = u'Всеволод Пападык'
-    USEREMAIL = 'seva213415@inbox.ru'
+    LOGIN = 'technopark25'
     PASSWORD = os.environ['PASSWORD']
     BLOG = 'Флудилка'
     TITLE = u'ЗаГоЛоВоК'
@@ -163,41 +159,41 @@ class ExampleTest(unittest.TestCase):
         )
 
     def tearDown(self):
-        self.driver.quit()
+        pass
+        #self.driver.quit()
 
     def test(self):
         auth_page = AuthPage(self.driver)
         auth_page.open()
 
         auth_form = auth_page.form
-        auth_form.open_form()
-        auth_form.set_login(self.USEREMAIL)
+        auth_form.set_login(self.LOGIN)
         auth_form.set_password(self.PASSWORD)
         auth_form.submit()
 
-        user_name = auth_page.top_menu.get_username()
-        self.assertEqual(self.USERNAME, user_name)
-
-        create_page = CreatePage(self.driver)
-        create_page.open()
-
-        create_form = create_page.form
-        create_form.blog_select_open()
-        create_form.blog_select_set_option(self.BLOG)
-        create_form.set_title(self.TITLE)
-        create_form.set_main_text(self.MAIN_TEXT)
-        create_form.set_unpublish()
-        create_form.submit()
-
-        topic_page = TopicPage(self.driver)
-        topic_title = topic_page.topic.get_title()
-        topic_text = topic_page.topic.get_text()
-        self.assertEqual(self.TITLE, topic_title)
-        self.assertEqual(self.MAIN_TEXT, topic_text)
-
-        blog_page = BlogPage(self.driver)
-        blog_page.topic.delete()
-        topic_title = blog_page.topic.get_title()
-        topic_text = blog_page.topic.get_text()
-        self.assertNotEqual(self.TITLE, topic_title)
-        self.assertNotEqual(self.MAIN_TEXT, topic_text)
+        # user_name = auth_page.top_menu.get_username()
+        # self.assertEqual(self.USERNAME, user_name)
+        #
+        # create_page = CreatePage(self.driver)
+        # create_page.open()
+        #
+        # create_form = create_page.form
+        # create_form.blog_select_open()
+        # create_form.blog_select_set_option(self.BLOG)
+        # create_form.set_title(self.TITLE)
+        # create_form.set_main_text(self.MAIN_TEXT)
+        # create_form.set_unpublish()
+        # create_form.submit()
+        #
+        # topic_page = TopicPage(self.driver)
+        # topic_title = topic_page.topic.get_title()
+        # topic_text = topic_page.topic.get_text()
+        # self.assertEqual(self.TITLE, topic_title)
+        # self.assertEqual(self.MAIN_TEXT, topic_text)
+        #
+        # blog_page = BlogPage(self.driver)
+        # blog_page.topic.delete()
+        # topic_title = blog_page.topic.get_title()
+        # topic_text = blog_page.topic.get_text()
+        # self.assertNotEqual(self.TITLE, topic_title)
+        # self.assertNotEqual(self.MAIN_TEXT, topic_text)
